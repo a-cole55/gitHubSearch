9dadc1d8ce9bc59c8c34fe72e785827fca74ecce
a-cole55 <arniqua_c@yahoo.com>
2022-09-04 11:56:27 -0500

    search bar styling changes

--- src/components/pages/SearchResults/Search.js
+++ src/components/pages/SearchResults/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Results from "../../Results";
 import "./Search.css";
 import logo from "../../../assets/github.png";
@@ -6,24 +6,35 @@ import { SearchOutlined } from '@ant-design/icons';
 
 
 function SearchResults() {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive(current => !current);
+  }
   return (
-    <div>
+    <div id="searchPage">
       <header>
         <nav>
           <img src={logo} alt="github logo" id="logo" />
-          <input type="text" name="userName" id="name" />
-          <button><SearchOutlined style={{fontSize:"16px"}} /></button>
+          <h6>GitHub</h6>
+          <div id="navSearch">
+            <input type="text" name="userName" id="query" 
+              onClick={handleClick}
+              style={{
+                borderColor: isActive ? "#38A6FF" : ''}} />
+            <button id="navSearchBTN"><SearchOutlined style={{fontSize:"18px"}} /></button>
+          </div>
             <ul>
                 <li><a href="/login"> Login</a></li>
                 <li><a href="/signup"> Signup</a></li>
             </ul>
         </nav>
     </header>
-
-        <h1>Search Results</h1>
-        <div>
-            <Results />
-        </div>
+    <h1>Search Results</h1>
+    <div className='pagination'>
+      <div>
+          <Results />
+      </div>
+    </div>
     </div>
   )
 }
