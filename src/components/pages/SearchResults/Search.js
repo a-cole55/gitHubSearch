df1ab9d89cae4cc6cbfe9d0c8147b2952b48082a
a-cole55 <arniqua_c@yahoo.com>
2022-09-04 02:29:39 -0500

    added nav bar to search results page

--- src/components/pages/SearchResults/Search.js
+++ src/components/pages/SearchResults/Search.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import Results from "../../Results"
+import Results from "../../Results";
+import "./Search.css";
+import logo from "../../../assets/github.png";
+import { SearchOutlined } from '@ant-design/icons';
+
 
 function SearchResults() {
   return (
     <div>
+      <header>
+        <nav>
+          <img src={logo} alt="github logo" id="logo" />
+          <input type="text" name="userName" id="name" />
+          <button><SearchOutlined style={{fontSize:"16px"}} /></button>
+            <ul>
+                <li><a href="/login"> Login</a></li>
+                <li><a href="/signup"> Signup</a></li>
+            </ul>
+        </nav>
+    </header>
+
         <h1>Search Results</h1>
         <div>
             <Results />
